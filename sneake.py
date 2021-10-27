9cf747b6d8dce8a3ab0de3400d41356857545e9b
K4B1R88 <88957506+K4B1R88@users.noreply.github.com>
2021-10-27 22:41:42 +0530

    Add files via upload

---
+++ sneake.py
@@ -0,0 +1,114 @@
+import webbrowser
+import sys
+import pyfiglet
+import colorama
+from colorama import Fore , Back , Style
+colorama.init(autoreset = True)
+
+
+
+
+
+				
+class start:
+	
+	def first(self):
+					print(f'''{Fore.GREEN}
+	▒█░▒█ ░█▀▀█ ▒█▀▀█ ▒█▀▀▀█ ▒█▀▀▄ ▒█▀▀▀ 
+	▒█▀▀█ ▒█▄▄█ ▒█░░░ ▒█░░▒█ ▒█░▒█ ▒█▀▀▀ 
+	▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█▄▄▄█ ▒█▄▄▀ ▒█▄▄▄ {Fore.YELLOW}
+	
+   ________´$$$$`_____________________________,,,_
+   _______´$$$$$$$`_________________________´$$$`
+   ________`$$$$$$$`______,,________,,_______´$$$$´
+   _________`$$$$$$$`____´$$`_____´$$`____´$$$$$´
+   __________`$$$$$$$`_´$$$$$`_´$$$$$`__´$$$$$$$´
+   ___________`$$$$$$$_$$$$$$$_$$$$$$$_´$$$$$$$´_
+   ____________`$$$$$$_$$$$$$$_$$$$$$$`´$$$$$$´_
+   ___,,,,,,______`$$$$$$_$$$$$$$_$$$$$$$_$$$$$$´_
+   _´$$$$$`____`$$$$$$_$$$$$$$_$$$$$$$_$$$$$$´_
+  ´$$$$$$$$$`´$$$$$$$_$$$$$$$_$$$$$$$_$$$$$´_
+  ´$$$$$$$$$$$$$$$$$$_$$$$$$$_$$$$$$$_$$$$$´_
+   ___`$$$$$$$$$$$$$$$_$$$$$$$_$$$$$$_$$$$$$´_
+   ______`$$$$$$$$$$$$$_$$$$$__$$_$$$$$$_$$´_
+   _______`$$$$$$$$$$$$,___,$$$$,_____,$$$$$´_
+   _________`$$$$$$$$$$$$$$$$$$$$$$$$$$$$$´_
+   __________`$$$$$$$$$$$$$$$$$$$$$$$$$$$´_
+   ____________`$$$$$$$$$$$$$$$$$$$$$$$$´_
+   _______________`$$$$$$$$$$$$$$$$$$$$´_
+''')
+					
+					print(" ")
+					print(f"           {Fore.GREEN} ::~:: {Fore.WHITE} CREATED BY HACODE  {Fore.GREEN}::~::")
+					print(f"         {Fore.GREEN}::~:: {Fore.WHITE}github.com/united-hackers {Fore.GREEN}::~::")
+					print(f''' {Fore.GREEN}
+		      [{Fore.RED}1{Fore.GREEN}]---START
+		      [{Fore.RED}2{Fore.GREEN}]---UPDATE
+		      [{Fore.RED}3{Fore.GREEN}]---ABOUT
+		      [{Fore.RED}4{Fore.GREEN}]---MORE
+		      [{Fore.RED}5{Fore.GREEN}]---FOLLOW
+		      [{Fore.RED}6{Fore.GREEN}]---VIDEO
+		      [{Fore.RED}7{Fore.GREEN}]---CHAT NOW
+		      [{Fore.RED}8{Fore.GREEN}]---RESTART
+		      [{Fore.RED}9{Fore.GREEN}]---EXIT ''')
+						
+						
+					c = " "
+					while(c != 9):
+						c = int(input(f'{Fore.GREEN}Select Option: {Fore.RED}'))
+						if c == 1:
+							webbrowser.open("https://youtube.com/channel/UCi00gqBm4n98-dg7ND7gsrg")
+						elif c == 2:
+							print("")
+							print(f"{Fore.YELLOW}This Tool Updating Very Soon")
+							print(f"{Fore.YELLOW}keep wait")
+							print("")
+						elif c == 3:
+							print('''
+							
+							
+░█████╗░██████╗░░█████╗░██╗░░░██╗████████╗
+██╔══██╗██╔══██╗██╔══██╗██║░░░██║╚══██╔══╝
+███████║██████╦╝██║░░██║██║░░░██║░░░██║░░░
+██╔══██║██╔══██╗██║░░██║██║░░░██║░░░██║░░░
+██║░░██║██████╦╝╚█████╔╝╚██████╔╝░░░██║░░░
+╚═╝░░╚═╝╚═════╝░░╚════╝░░╚═════╝░░░░╚═╝░░░          
+                     
+THIS TOOLS IS ONLY FOR EDUCATIONAL PURPOSE SO'
+IM NOT RESPONSIBLE IF YOU DO ANY ILLEGAL THINGS'
+THANKS FOR READING SUBSCRIBE {HACODE}'
+HAVE A GOOD DAY BUDDIE :)
+''')
+						elif c == 4:
+						 	print(f"{Fore.YELLOW}Comming More Surprices for you")
+						elif c == 5:
+							webbrowser.open("https://www.instagram.com/hacode_88/")
+						elif c == 6:
+							webbrowser.open("https://youtube.com/channel/UCi00gqBm4n98-dg7ND7gsrg")
+						elif c == 7:
+							webbrowser.open("https://chat.whatsapp.com/JnrKsQW55tFAvq3iTgO2Ut")
+						elif c == 8:
+							start.first("kriss")
+						elif c == 9:
+							exit()
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+start.first("kriss")
+	
+		
+		
+	  
+	
+			
+	
